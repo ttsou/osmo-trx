46f288a8eb12a19506de33ce11cec34a322b9b87
Tom Tsou <tom.tsou@ettus.com>
2017-06-15 15:31:08 -0700

    uhd: Fix Tx-RX timing offset setting
    Integer timestamp offset was set to zero due to bad cast-operator
    precedence.

--- Transceiver52M/UHDDevice.cpp
+++ Transceiver52M/UHDDevice.cpp
@@ -437,7 +437,7 @@ void uhd_device::set_rates()
 	tx_rate = usrp_dev->get_tx_rate();
 	rx_rate = usrp_dev->get_rx_rate();
 
-	ts_offset = (TIMESTAMP) desc.offset * rx_rate;
+	ts_offset = static_cast<TIMESTAMP>(desc.offset * rx_rate);
 	LOG(INFO) << "Rates configured for " << desc.str;
 }
 
@@ -828,7 +828,6 @@ bool uhd_device::stop()
 		uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
 
 	usrp_dev->issue_stream_cmd(stream_cmd);
-
 	async_event_thrd->cancel();
 	async_event_thrd->join();
 	delete async_event_thrd;
